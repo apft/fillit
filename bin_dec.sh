10b8be291b3153edb37acb5d0995a8bb73e12857
Alexandre PION <apion@student.42.fr>
2018-12-19 14:47:58 +0100

    use unsigned short values in tiles definition

--- bin_dec.sh
+++ bin_dec.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-values=$(grep -E "{.+/" parser.c | cut -d ' ' -f3)
+old_IFS=$IFS
+IFS=$'\n'
 
-for i in $values
+for i in $(grep -E '{.+/' parser.c)
 do
-	n=$i
-	sign=${n:0:1}
-	[ $sign -eq 1 ] && num=${n:1} || num=${n:0}
-	num=$((2#$num))
-	[ $sign -eq 1 ] && num=$(($num-32768))
-	echo "$i : $num"
+	echo "============="
+	echo $i | xargs
+	tile=$(echo $i | cut -d ' ' -f3)
+	shif=$(echo $i | cut -d ' ' -f4)
+	for j in $tile $shif
+	do
+		n=$j
+		sign=${n:0:1}
+		[ $sign -eq 1 ] && num=${n:1} || num=${n:0}
+		num=$((2#$num))
+		#[ $sign -eq 1 ] && num=$(($num-32768))
+		echo "$j : $num"
+	done
 done
+IFS=${old_IFS}
