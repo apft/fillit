ad048c59b72cbd2c712c105715866c5ce6f32d64
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2018-12-20 14:27:56 +0100

    Add print_bits function to dbg_utils

--- dbg_utils.c
+++ dbg_utils.c
@@ -6,11 +6,22 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 18:20:30 by apion             #+#    #+#             */
-/*   Updated: 2018/12/20 12:18:43 by apion            ###   ########.fr       */
+/*   Updated: 2018/12/20 14:26:42 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dbg_utils.h"
+
+void	print_bits(unsigned short nb)
+{
+	unsigned long	count;
+	unsigned short	mask;
+
+	count = sizeof(nb) * 8;
+	mask = 1 << (sizeof(nb) * 8 - 1);
+	while (count-- > 0) 
+		write (1, (mask >> (15 - count) & nb) ? "1" : "0", 1);
+}
 
 void	dbg_print_nbr(const char *str, const int n)
 {
@@ -40,21 +51,13 @@ void	dbg_print_tiles(const t_tile *tiles, const int k)
 		ft_putnbr(i);
 		ft_putstr(" ==========");
 		ft_putstr("\nline0: ");
-		if (tiles[i].line0 < 32768)
-			ft_putnbr(0);
-		ft_putnbr_base(tiles[i].line0, "01");
+		print_bits(tiles[i].line0);
 		ft_putstr("\nline1: ");
-		if (tiles[i].line1 < 32768)
-			ft_putnbr(0);
-		ft_putnbr_base(tiles[i].line1, "01");
+		print_bits(tiles[i].line1);
 		ft_putstr("\nline2: ");
-		if (tiles[i].line2 < 32768)
-			ft_putnbr(0);
-		ft_putnbr_base(tiles[i].line2, "01");
+		print_bits(tiles[i].line2);
 		ft_putstr("\nline3: ");
-		if (tiles[i].line3 < 32768)
-			ft_putnbr(0);
-		ft_putnbr_base(tiles[i].line3, "01");
+		print_bits(tiles[i].line3);
 		ft_putendl(0);
 		i++;
 	}
