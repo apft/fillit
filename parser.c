57539c3519ae53fe43e328c69e83665bc63b7dc3
Alexandre PION <apion@student.42.fr>
2018-12-21 11:02:29 +0100

    fix formula for width

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 17:49:32 by apion             #+#    #+#             */
-/*   Updated: 2018/12/20 15:26:23 by apion            ###   ########.fr       */
+/*   Updated: 2018/12/21 11:01:56 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,9 +66,9 @@ static int	create_tile(t_tile *tetrimino, unsigned short tile)
 	{
 		tetrimino->lines[i] = ((tile << (4 * i)) >> 12) << 12;
 		tetrimino->height += (tetrimino->lines[i] != 0);
-		if (tetrimino->width < active_bit(tetrimino->lines[i]))
-			tetrimino->width = active_bit(tetrimino->lines[i]);
+		tetrimino->width |= tetrimino->lines[i];
 	}
+	tetrimino->width = active_bit(tetrimino->width);
 	return (0);
 }
 
