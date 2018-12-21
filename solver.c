07740747d32b82bb60829701060898ff9a288aa3
Alexandre PION <apion@student.42.fr>
2018-12-21 13:36:49 +0100

    remove a lot of debug code in solver

--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 14:11:19 by apion             #+#    #+#             */
-/*   Updated: 2018/12/21 13:24:11 by apion            ###   ########.fr       */
+/*   Updated: 2018/12/21 13:35:40 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,19 @@ static int	add_tile(t_map *map, t_tile *tile)
 	int		y;
 
 	err = 0;
-	ft_putendl("\e[1;4mADD.start\e[0m");
-	dbg_print_map(map);
-	dbg_print_tile(tile, 0);
 	h = tile->height;
 	x = tile->x;
 	y = tile->y;
 	if ((y + tile->height) > map->n)
 		return (0);
 	while (h-- && !err)
-	{
 		err = map->lines[y + h] & (tile->lines[h] >> x);
-		dbg_print_nbr("err", err);
-		print_bits(map->lines[y + h]);
-		print_bits(tile->lines[h] >> x);
-	}
 	if (!err)
 	{
-		ft_putendl("add tile");
 		h = tile->height;
 		while (h--)
 			map->lines[h + y] |= (tile->lines[h] >> x);
 	}
-	ft_putendl("\e[1;4mADD.end\e[0m");
-	dbg_print_map(map);
 	return (!err);
 }
 
@@ -56,21 +45,15 @@ static void	remove_tile(t_map *map, t_tile *tile)
 	int		x;
 	int		y;
 
-	ft_putendl("\e[1;4mREMOVE.start\e[0m");
-	dbg_print_map(map);
 	h = tile->height;
 	x = tile->x;
 	y = tile->y;
 	while (h--)
 		map->lines[h + y] ^= (tile->lines[h] >> x);
-	ft_putendl("\e[1;4mREMOVE.end\e[0m");
-	dbg_print_map(map);
 }
 
 static void	move_tile(int n, t_tile *tile)
 {
-	ft_putendl("\e[1;4mMOVE.start\e[0m");
-	dbg_print_tile(tile, -1);
 	if (tile->x + tile->width < n)
 		tile->x++;
 	else
@@ -78,8 +61,6 @@ static void	move_tile(int n, t_tile *tile)
 		tile->x = 0;
 		tile->y++;
 	}
-	ft_putendl("\e[1;4mMOVE.end\e[0m");
-	dbg_print_tile(tile, -1);
 }
 
 static char	*init_str(int n)
@@ -120,18 +101,13 @@ static int	set_char(char *str, t_tile *tile, int i, t_map *map)
 	h = 0;
 	while (h < tile->height)
 	{
-		print_bits(tile->lines[h]);
 		start = x + (y + h) * (map->n + 1);
-		dbg_print_nbr("start", start);
 		j = 0;
 		mask = 1 << (sizeof(mask) * 8 - 1);
 		while (j < 4)
 		{
 			if (tile->lines[h] & (mask >> j))
-			{
-				dbg_print_nbr("print_pos", start+j);
 				*(str + start + j) = 'A' + i;
-			}
 			j++;
 		}
 		h++;
@@ -145,7 +121,6 @@ static int	fillit(t_map *map, t_tile *tiles, int k, int i)
 	int				fill;
 	int				y;
 
-	dbg_print_nbr("fillit for tile", i);
 	if (i == k)
 	{
 		out = init_str(map->n);
@@ -157,7 +132,6 @@ static int	fillit(t_map *map, t_tile *tiles, int k, int i)
 	{
 		if ((fill = add_tile(map, &tiles[i])) && fillit(map, tiles, k, i + 1))
 		{
-			dbg_print_tile(&tiles[i], i);
 			set_char(out, &tiles[i], i, map);
 			if (i == 0)
 				ft_putstr(out);
@@ -198,14 +172,10 @@ int			solver(t_tile *tiles, int k)
 	dbg_print_nbr("k", k);
 	while (map.n * map.n < 4 * k)
 		map.n++;
-	dbg_print_nbr("n", map.n);
 	while (!(fill = fillit(&map, tiles, k, 0)))
 	{
-		dbg_print_nbr("fill", fill);
+		dbg_print_nbr("n", map.n);
 		map.n++;
-		dbg_print_nbr("new n", map.n);
 	}
-		dbg_print_nbr("last n", map.n);
-		dbg_print_nbr("last fill", fill);
 	return (0);
 }
