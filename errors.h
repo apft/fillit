a2c7f054c1f4c33b397448807e15df9388454854
Alexandre PION <apion@student.42.fr>
2018-12-22 11:19:36 +0100

    add error header

---
+++ errors.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/21 18:50:47 by apion             #+#    #+#             */
+/*   Updated: 2018/12/21 18:59:42 by apion            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	ERRORS_H
+# define ERRORS_H
+
+# define ERR_OPEN 1
+# define ERR_READ 2
+# define ERR_MALLOC 3
+# define ERR_FILE 4
+# define ERR_FILE_SIZE 5
+# define ERR_TILE 6
+# define ERR_CHAR 7
+
+#endif
